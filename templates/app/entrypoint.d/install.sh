af582ec5b9006e0fe0fed023bc14d96df0c70263
jindong <jindong05@126.com>
2025-05-22 00:46:50 +0800

    fix: node-gyp支持

--- templates/app/entrypoint.d/install.sh
+++ templates/app/entrypoint.d/install.sh
@@ -4,6 +4,13 @@ set -eu
 cd /root/src
 echo "PWD "$PWD
 
+####################### SQLite环境支持 Start #######################
+# 安装make,gcc,gdb等，node-gyp无法编译
+apt update
+apt -y install build-essential
+####################### SQLite环境支持 End #######################
+
+# npm install
 echo "Running npm install..."
 npm install
 
