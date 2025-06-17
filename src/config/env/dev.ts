18c5c98780350c078f8de7f32e951a1a4bed383f
jindong <jindong05@126.com>
2025-06-17 16:32:59 +0800

    chore: redis useMock

--- src/config/env/dev.ts
+++ src/config/env/dev.ts
@@ -22,11 +22,12 @@ export = {
     },
 
     redis: {
-        port: 6379, // Redis port
-        host: '127.0.0.1', // Redis host
-        family: 4, // 4 (IPv4) or 6 (IPv6)
-        db: 6,
-        password: 'nebula123',
+        useMock: true,
+        // port: 6379, // Redis port
+        // host: '127.0.0.1', // Redis host
+        // family: 4, // 4 (IPv4) or 6 (IPv6)
+        // db: 6,
+        // password: 'nebula123',
     },
 
     // mongodb: {
